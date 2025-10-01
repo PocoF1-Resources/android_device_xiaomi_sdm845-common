50671db39c9b2845776f0699385d86e221bf5381
Dheeraj R Shrivatsa <Dheeraj3031A@users.noreply.github.com>
2025-10-01 21:36:11 +0530

    Revert "sdm845-common: Flash dummy super image if needed"
    This reverts commit 31979838eb8d57a6ccdd14ec4eecf865eca37c2c.

--- sdm845.mk
+++ sdm845.mk
@@ -222,8 +222,6 @@ PRODUCT_PACKAGES += \
     vendor_firmware_mnt_mountpoint
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/partitions/flash_super_dummy.sh:install/bin/flash_super_dummy.sh
 
 # Power
 PRODUCT_PACKAGES += \
