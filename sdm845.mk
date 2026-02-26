db40871bbcdc67b1ca15a73f378527baa4083f34
me-cafebabe <me-cafebabe@users.noreply.github.com>
2026-02-26 19:56:53 +0000

    sdm845-common: Disable VINTF kernel requirements
    Change-Id: I12a75936976078a29fa0d15e24ced97d1ad9e4c1

--- sdm845.mk
+++ sdm845.mk
@@ -14,7 +14,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
